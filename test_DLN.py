2624e5d0e539ce4b53b53139913d39796510db73
asoderlind <axesode@gmail.com>
2025-03-11 14:18:00 +0100

    update dln

--- test_DLN.py
+++ test_DLN.py
@@ -1,9 +1,10 @@
 import glob
 import random
-from notebooks.utils import compare_images, enhance_image
+from notebooks.utils import compare_images, enhance_image, get_dln_model
 from PIL import Image
 
 dataset_path = "../yolo-testing/datasets/exDark-yolo"
 filename = random.sample(glob.glob(f"{dataset_path}/images/train/*.jpg"), 1)[0]
-image_out = enhance_image(filename)
+dln = get_dln_model()
+image_out = enhance_image(filename, dln)
 compare_images(Image.open(filename), image_out)
