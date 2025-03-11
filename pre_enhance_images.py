42e20b2f37c1c3945002621f86d375356c78643b
asoderlind <axesode@gmail.com>
2025-03-11 13:52:44 +0100

    fix pre-enhancer

--- pre_enhance_images.py
+++ pre_enhance_images.py
@@ -1,8 +1,12 @@
 import os
 import glob
-from notebooks.utils import enhance_image
 import cv2
+import numpy as np
+from PIL import Image
 from tqdm import tqdm
+from ultralytics.DLN.model import DLN
+import torchvision.transforms as transforms
+import torch
 
 dataset = "exDark-yolo"
 
@@ -27,6 +31,30 @@ print(f"Found {len(raw_train_images)} training images and {len(raw_val_images)} 
 # Create target directories if they don't exist
 os.makedirs(target_train_folder, exist_ok=True)
 os.makedirs(target_val_folder, exist_ok=True)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu")
+model = DLN()
+model = torch.nn.DataParallel(model)
+model.load_state_dict(torch.load("ultralytics/DLN/DLN_finetune_LOL.pth", map_location=lambda storage, loc: storage))
+model.eval()
+model.to(device)
+
+
+def enhance_image(filename, dln_chpt="ultralytics/DLN/DLN_finetune_LOL.pth"):
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu")
+    trans = transforms.ToTensor()
+    torch.manual_seed(0)
+    with torch.no_grad():
+        test_image_in = Image.open(filename).convert("RGB")
+        test_image_in_tensor = trans(test_image_in)
+        test_image_in_tensor = test_image_in_tensor.unsqueeze(0)
+        test_image_in_tensor = test_image_in_tensor.to(device)  # shape [1, 3, 720, 1280]
+        prediction = model(test_image_in_tensor)  # shape [1, 3, 720, 1280]
+        prediction = prediction.data[0].cpu().numpy().transpose((1, 2, 0))
+        prediction = prediction * 255.0
+        prediction = prediction.clip(0, 255)  # shape [720, 1280, 3]
+    return np.uint8(prediction)
+
 
 # Enhance training images with progress bar
 print("Enhancing training images:")
