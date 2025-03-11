f94cbf792fd67c41db8f75cec87e3bc6d6e66188
asoderlind <axesode@gmail.com>
2025-03-11 15:23:56 +0100

    add notebooks and dln wrangling

--- test_DLN.py
+++ test_DLN.py
@@ -5,6 +5,9 @@ from PIL import Image
 
 dataset_path = "../yolo-testing/datasets/exDark-yolo"
 filename = random.sample(glob.glob(f"{dataset_path}/images/train/*.jpg"), 1)[0]
+filename = "../yolo-testing/datasets/exDark-yolo/images/train/2015_03267.jpg"
+# filename = "../yolo-testing/datasets/exDark-yolo/images/train/2015_01388.jpg"
+print(filename)
 dln = get_dln_model()
 image_out = enhance_image(filename, dln)
 compare_images(Image.open(filename), image_out)
