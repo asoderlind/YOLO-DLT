9e1798e81e190e72df925196378cce8173d56405
asoderlind <axesode@gmail.com>
2025-03-11 13:34:21 +0100

    fix pre-enhancer

--- pre_enhance_images.py
+++ pre_enhance_images.py
@@ -4,18 +4,25 @@ from notebooks.utils import enhance_image
 import cv2
 from tqdm import tqdm
 
+dataset = "exDark128-yolo"
+
 # Define paths
-dataset_path = "../yolo-testing/datasets/exDark128-yolo/images"
-target_train_folder = "../yolo-testing/datasets/exDark128-yolo/enhanced_images/train"
-target_val_folder = "../yolo-testing/datasets/exDark128-yolo/enhanced_images/val"
+dataset_path = f"../yolo-testing/datasets/{dataset}/images"
+target_train_folder = f"../yolo-testing/datasets/{dataset}/images/train_enhanced"
+target_val_folder = f"../yolo-testing/datasets/{dataset}/images/val_enhanced"
 
 # Create target directories if they don't exist
 os.makedirs(target_train_folder, exist_ok=True)
 os.makedirs(target_val_folder, exist_ok=True)
 
+formats = ["jpg", "jpeg", "png", "JPG", "JPEG", "PNG"]
+
 # Get raw image lists
-raw_train_images = glob.glob(f"{dataset_path}/train/*.jpg")
-raw_val_images = glob.glob(f"{dataset_path}/val/*.jpg")
+raw_train_images = []
+raw_val_images = []
+for format in formats:
+    raw_train_images += glob.glob(f"{dataset_path}/train/*.{format}")
+    raw_val_images += glob.glob(f"{dataset_path}/val/*.{format}")
 
 print(f"Found {len(raw_train_images)} training images and {len(raw_val_images)} validation images.")
 
@@ -23,10 +30,20 @@ print(f"Found {len(raw_train_images)} training images and {len(raw_val_images)} 
 print("Enhancing training images:")
 for image in tqdm(raw_train_images, desc="Train", unit="img"):
     enhanced_image = enhance_image(image)
-    cv2.imwrite(f"{target_train_folder}/{os.path.basename(image)}", enhanced_image)
+    if enhanced_image is not None:
+        path = f"{target_train_folder}/{os.path.basename(image)}"
+        if os.path.exists(path):
+            print(f"Skipping {path}")
+            continue
+        cv2.imwrite(path, enhanced_image)
 
 # Enhance validation images with progress bar
 print("Enhancing validation images:")
 for image in tqdm(raw_val_images, desc="Validation", unit="img"):
     enhanced_image = enhance_image(image)
-    cv2.imwrite(f"{target_val_folder}/{os.path.basename(image)}", enhanced_image)
+    if enhanced_image is not None:
+        path = f"{target_val_folder}/{os.path.basename(image)}"
+        if os.path.exists(path):
+            print(f"Skipping {path}")
+            continue
+        cv2.imwrite(path, enhanced_image)
