196fb284502cc622072892fce4bf03e4d731f7b3
asoderlind <axesode@gmail.com>
2025-03-13 08:06:27 +0100

    some mods

--- feature_enhancer_train.py
+++ feature_enhancer_train.py
@@ -1,27 +1,72 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.yaml")
 
+# With training first layer at same time as rest of net
+'''
 fe = True
-epochs = 1
-lc = 10.0
+epochs = 100
+lcs = [0.2, 0.3, 0.4]
 
-model.train(
-    data="../ultralytics/cfg/datasets/exDark-yolo.yaml",
-    epochs=epochs,
-    batch=16,
-    pretrained=True,
-    optimizer="auto",
-    device="cuda",
-    use_fe=fe,
-    augment=False,
-    mosaic=0.0,
-    translate=0.0,
-    scale=0.0,
-    shear=0.0,
-    perspective=0.0,
-    fliplr=0.0,
-    val=False,
-    lambda_c=lc,
-    name=f"exDark128-{'fe-' if fe else ''}{epochs}-allLoss-lc{lc}-Auto-noAug-preLoad",
-)
+for lc in lcs:
+    model = YOLO("yolo11n.yaml")
+    model.train(
+        data="../ultralytics/cfg/datasets/exDark-yolo.yaml",
+        use_fe=fe,
+        epochs=epochs,
+        batch=16,
+        augment=True,
+        device="cuda",
+        lambda_c=lc,
+        optimizer="auto",
+        name=f"exDark-{'fe-' if fe else ''}{epochs}-allLoss-lc{lc}-Auto-aug-preLoad"
+    )
+
+
+'''
+# With training only first layer then freezing
+fe = True
+epochs_list = [10]
+lcs = [0.5]
+
+for epochs in epochs_list:
+    for lc in lcs:
+        pathConv1 = f"exDark-conv1-e{epochs}-lc{lc}"
+        model = YOLO("yolo11n.yaml")
+        model.train(
+            data="../ultralytics/cfg/datasets/exDark-yolo.yaml",
+            epochs=epochs,
+            batch=16,
+            pretrained=True,
+            optimizer="auto",
+            device="cuda",
+            use_fe=fe,
+            val=False,
+            lambda_c=lc,
+            augment=True,
+            box=0.0,
+            cls=0.0,
+            dfl=0.0,
+            save_json=True,
+            name=pathConv1
+        )
+
+        freezeNum = 1
+        model=YOLO(f"runs/detect/{pathConv1}/weights/last.pt")
+        model.train(
+            data="../ultralytics/cfg/datasets/exDark-yolo.yaml",
+            epochs=100,
+            batch=16,
+            pretrained=False,
+            optimizer="auto",
+            device="cuda",
+            use_fe=False,
+            val=True,
+            lambda_c=lc,
+            augment=True,
+            name=f"{pathConv1}-freeze{freezeNum}-e100_",
+            save_json=True,
+            freeze=freezeNum
+        )
+
+
+
