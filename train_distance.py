483ab9363d22098645535bf952f05ae37a2db32a
asoderlind <axesode@gmail.com>
2025-03-18 10:56:51 +0100

    rename files

--- train_distance.py
+++ train_distance.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+<<<<<<< HEAD
 import torch
 
 
@@ -80,3 +81,37 @@ train_with_distance(
     freeze=23,
 )
 """
+=======
+
+
+# Defaults
+model_path = "yolo11n.pt"
+model = YOLO(model_path)
+data_path = "waymo128-noConfidence.yaml"
+# data_path = "coco8-dist.yaml"
+device = "mps"
+use_fe = False
+epochs = 30
+opt = "auto"
+
+
+use_dists = [True]
+augs = [True]
+
+for use_dist in use_dists:
+    for aug in augs:
+        model.train(
+            data=data_path,
+            batch=2,
+            epochs=epochs,
+            device=device,
+            optimizer=opt,
+            use_fe=use_fe,
+            mosaic=1.0 if aug else 0.0,
+            translate=0.1 if aug else 0.0,
+            scale=0.5 if aug else 0.0,
+            name=f"{data_path}-{model_path}-{epochs}e-{'dist' if use_dist else 'noDist'}-{'fe' if use_fe else 'noFe'}-{'aug' if aug else 'noAug'}-{opt}",
+            iou_type="ciou",
+            use_dist=use_dist,
+        )
+>>>>>>> 110dccde (rename files)
--- train_feature_enhancer.py
+++ train_feature_enhancer.py
@@ -1,61 +1,30 @@
 from ultralytics import YOLO
 
-""" might resume this one later
-# Without fe
-fe = False
-epochs = 200
-lc = 0.0
-
-data = "exDark-yolo.yaml"
-model_name = "dlt-models/yolo11n-SPDConv-3.yaml"
-model = YOLO(model_name)
-
-model.train(
-    data=data,
-    use_fe=fe,
-    epochs=epochs,
-    augment=True,
-    device="cuda",
-    lambda_c=lc,
-    optimizer="auto",
-    name=f"{model_name}-{data}-{'fe-' if fe else ''}-e{epochs}-allLoss-lc{lc}-Auto-aug-preLoad",
-)
-"""
 
 # With training first layer at same time as rest of net
 fe = True
 epochs = 200
-lc = 0.5
-data = "exDark-yolo.yaml"
-model_name = "dlt-models/yolo11n-SPDConv-3.yaml"
-model = YOLO(
-    "runs/detect/dlt-models/yolo11n-SPDConv-3.yaml-exDark-yolo.yaml-fe--e200-allLoss-lc0.5-Auto-aug-preLoad/weights/last.pt"
-)
+lcs = 0.5
+
+model = YOLO("yolo11n.yaml")
 model.train(
-    data=data,
+    data="../ultralytics/cfg/datasets/bdd100k_night.yaml",
     use_fe=fe,
     epochs=epochs,
-    resume=True,
+    batch=16,
     augment=True,
     device="cuda",
     lambda_c=lc,
     optimizer="auto",
-    name=f"{model_name}-{data}{'-fe-' if fe else '-noFe-'}e{epochs}-allLoss-lc{lc}-Auto-aug-preLoad_",
+    name=f"bdd100k_night-{'fe-' if fe else ''}{epochs}-allLoss-lc{lc}-Auto-aug-preLoad",
 )
 
-"""
+
 # With training only first layer then freezing
-epochs = 10
-fe = True
-lc = 0.5
-
-model_name = "dlt-models/yolo11n-SPDConv-3.yaml"
-data = "exDark-yolo.yaml"
-
-pathConv1 = f"{model_name}-{data}-conv1-e{epochs}-lc{lc}"
-model = YOLO(model_name)
+pathConv1 = f"bdd100k_night-conv1-e10-lc{lc}"
+model = YOLO("yolo11n.yaml")
 model.train(
-    data=data,
+    data="../ultralytics/cfg/datasets/exDark-yolo.yaml",
     epochs=10,
     batch=16,
     pretrained=True,
@@ -68,17 +37,16 @@ model.train(
     box=0.0,
     cls=0.0,
     dfl=0.0,
+    save_json=True,
     name=pathConv1,
 )
-"""
 
-"""
 freezeNum = 1
-epochs = 200
 model = YOLO(f"runs/detect/{pathConv1}/weights/last.pt")
 model.train(
-    data=data,
-    epochs=epochs,
+    data="../ultralytics/cfg/datasets/bdd100k_night.yaml",
+    epochs=200,
+    batch=16,
     pretrained=False,
     optimizer="auto",
     device="cuda",
@@ -86,8 +54,7 @@ model.train(
     val=True,
     lambda_c=lc,
     augment=True,
-    name=f"{pathConv1}-freeze{freezeNum}-e{epochs}_",
+    name=f"{pathConv1}-freeze{freezeNum}-e200_",
     save_json=True,
     freeze=freezeNum,
 )
-"""
