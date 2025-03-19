3b56f2301b431ece9e5e38e62323fa6251543be2
asoderlind <axesode@gmail.com>
2025-03-19 09:08:11 +0100

    update train distance runs

--- train_distance.py
+++ train_distance.py
@@ -3,31 +3,28 @@ from ultralytics import YOLO
 
 # Defaults
 model_path = "yolo11n.pt"
-model = YOLO(model_path)
 data_path = "waymo-noConf.yaml"
 device = "cuda"
 use_fe = False
-epochs = 200
-opt = "SGD"
-
+epochs = 150
 
 use_dists = [True, False]
-augs = [True, False]
-dists = [0.1, 0.5, 1.0, 2.0]
+dists = [10.0]
 
 for d in dists:
     for use_dist in use_dists:
-        for aug in augs:
-            model.train(
-                data=data_path,
-                epochs=epochs,
-                device=device,
-                optimizer=opt,
-                use_fe=use_fe,
-                mosaic=1.0 if aug else 0.0,
-                translate=0.1 if aug else 0.0,
-                scale=0.5 if aug else 0.0,
-                name=f"{data_path}-{model_path}-{epochs}e-{'dist' if use_dist else 'noDist'}-{'fe' if use_fe else 'noFe'}-{'aug' if aug else 'noAug'}-{opt}-d{d}",
-                iou_type="ciou",
-                use_dist=use_dist,
-            )
+        model = YOLO(model_path)
+        model.train(
+            data=data_path,
+            epochs=epochs,
+            device=device,
+            optimizer="SGD",
+            momentum=0.9,
+            lr0=0.01,
+            warmup_bias_lr=0.0,
+            use_fe=use_fe,
+            name=f"{data_path}-{model_path}-{epochs}e-{'dist' if use_dist else 'noDist'}-d{d}_",
+            iou_type="ciou",
+            use_dist=use_dist,
+            dist=d,
+        )
