ad48ee1428390d27a88505c10e0f2608a1cdb4d5
asoderlind <axesode@gmail.com>
2025-03-21 19:02:50 +0100

    some mods3

--- train_distance.py
+++ train_distance.py
@@ -6,25 +6,31 @@ model_path = "yolo11n.pt"
 data_path = "kitti.yaml"
 device = "cuda"
 use_fe = False
-epochs = 10
+epochs = 100
+optimizer="SGD"
+noAug=False
+use_dist = False
+dists=[0.05, 0.1, 0.15]
 
-use_dists = [True]
-dists = [0.1]
+#model_path=f"{name}/weights/last.py"
 
 for d in dists:
-    for use_dist in use_dists:
-        model = YOLO(model_path)
-        model.train(
-            data=data_path,
-            epochs=epochs,
-            device=device,
-            optimizer="SGD",
-            momentum=0.9,
-            lr0=0.01,
-            warmup_bias_lr=0.0,
-            use_fe=use_fe,
-            name=f"{data_path}-{model_path}-{epochs}e-{'dist' if use_dist else 'noDist'}-d{d}_",
-            iou_type="ciou",
-            use_dist=use_dist,
-            dist=d,
-        )
+    model = YOLO(model_path)
+    name=f"{data_path}-{model_path}-{epochs}e-{optimizer}-{'noAug' if noAug else 'aug'}-{'dist' if use_dist else 'noDist'}-noAug-d{d}_"
+    model.train(
+        data=data_path,
+        epochs=epochs,
+        device=device,
+        optimizer=optimizer,
+        momentum=0.9,
+        lr0=0.01,
+        warmup_bias_lr=0.0,
+        use_fe=use_fe,
+        name=name,
+        iou_type="ciou",
+        mosaic=0.0 if noAug else 1.0,
+        scale=0.0 if noAug else 0.5,
+        use_dist=use_dist,
+        dist=d,
+        # resume = True
+    )
