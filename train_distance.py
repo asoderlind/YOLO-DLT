6e5d8b6b61c67e4caabd14bbc383f77b39b4e820
asoderlind <axesode@gmail.com>
2025-03-23 16:06:23 +0100

    some mods4

--- train_distance.py
+++ train_distance.py
@@ -6,32 +6,31 @@ model_path = "yolo11n.pt"
 data_path = "kitti.yaml"
 device = "cuda"
 use_fe = False
-epochs = 100
+epochs = 200
 optimizer = "SGD"
-scales = [0.0, 0.25, 0.5]
-mosaics = [0.0, 0.5, 1.0]
+scale = 0.0
+mosaic = 1.0
 use_dist = True
+d=0.35
 
 # model_path=f"{name}/weights/last.py"
 
-for scale in scales:
-    for mosaic in mosaics:
-        model = YOLO(model_path)
-        name = f"{data_path}-{model_path}-{epochs}e-{optimizer}-scale{scale}-mosaic{mosaic}-d0.1_"
-        model.train(
-            data=data_path,
-            epochs=epochs,
-            device=device,
-            optimizer=optimizer,
-            momentum=0.9,
-            lr0=0.01,
-            warmup_bias_lr=0.0,
-            use_fe=use_fe,
-            name=name,
-            iou_type="ciou",
-            mosaic=mosaic,
-            scale=scale,
-            use_dist=use_dist,
-            dist=0.1,
-            # resume = True
-        )
+model = YOLO(model_path)
+name = f"{data_path}-{model_path}-{epochs}e-{optimizer}-scale{scale}-mosaic{mosaic}-d{d}_"
+model.train(
+    data=data_path,
+    epochs=epochs,
+    device=device,
+    optimizer=optimizer,
+    momentum=0.9,
+    lr0=0.01,
+    warmup_bias_lr=0.0,
+    use_fe=use_fe,
+    name=name,
+    iou_type="ciou",
+    mosaic=mosaic,
+    scale=scale,
+    use_dist=use_dist,
+    dist=d,
+    # resume = True
+)
