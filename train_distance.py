e7fcea810c89fb1423662a263cd426a3f2fb689f
asoderlind <axesode@gmail.com>
2025-03-23 09:04:30 +0100

    prepare for testing augs

--- train_distance.py
+++ train_distance.py
@@ -7,30 +7,31 @@ data_path = "kitti.yaml"
 device = "cuda"
 use_fe = False
 epochs = 100
-optimizer="SGD"
-noAug=False
+optimizer = "SGD"
+scales = [0.0, 0.25, 0.5]
+mosaics = [0.0, 0.25, 0.5, 1.0]
 use_dist = True
-dists=[0.05, 0.1, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60]
 
-#model_path=f"{name}/weights/last.py"
+# model_path=f"{name}/weights/last.py"
 
-for d in dists:
-    model = YOLO(model_path)
-    name=f"{data_path}-{model_path}-{epochs}e-{optimizer}-{'noAug' if noAug else 'aug'}-{'dist' if use_dist else 'noDist'}-noAug-d{d}_"
-    model.train(
-        data=data_path,
-        epochs=epochs,
-        device=device,
-        optimizer=optimizer,
-        momentum=0.9,
-        lr0=0.01,
-        warmup_bias_lr=0.0,
-        use_fe=use_fe,
-        name=name,
-        iou_type="ciou",
-        mosaic=0.0 if noAug else 1.0,
-        scale=0.0 if noAug else 0.5,
-        use_dist=use_dist,
-        dist=d,
-        # resume = True
-    )
+for scale in scales:
+    for mosaic in mosaics:
+        model = YOLO(model_path)
+        name = f"{data_path}-{model_path}-{epochs}e-{optimizer}-scale{scale}-mosaic{mosaic}-d0.1_"
+        model.train(
+            data=data_path,
+            epochs=epochs,
+            device=device,
+            optimizer=optimizer,
+            momentum=0.9,
+            lr0=0.01,
+            warmup_bias_lr=0.0,
+            use_fe=use_fe,
+            name=name,
+            iou_type="ciou",
+            mosaic=mosaic,
+            scale=scale,
+            use_dist=use_dist,
+            dist=0.1,
+            # resume = True
+        )
