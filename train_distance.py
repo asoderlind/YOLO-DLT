670e975aad99500680c1b8383cd56180556465c8
asoderlind <axesode@gmail.com>
2025-03-25 12:31:27 +0100

    some mods7

--- train_distance.py
+++ train_distance.py
@@ -4,7 +4,7 @@ import torch
 
 # Defaults
 model_path = "yolo11n.pt"
-data_path = "kitti-mini.yaml"
+data_path = "kitti.yaml"
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu"
 use_fe = False
 epochs = 100
@@ -16,23 +16,22 @@ d = 0.35
 
 # model_path = "/home/phoawb/repos/YOLO-DLT/runs/detect/kitti.yaml-yolo11l.pt-200e-SGD-scale0.0-mosaic1.0-d0.35_14/weights/last.pt"
 # model_path=f"{name}/weights/last.py"
-for d in ds:
-    model = YOLO(model_path)
-    name = f"{data_path}-{model_path}-{epochs}e-{optimizer}-scale{scale}-mosaic{mosaic}-d{d}_"
-    model.train(
-        data=data_path,
-        epochs=epochs,
-        device=device,
-        optimizer=optimizer,
-        momentum=0.9,
-        lr0=0.01,
-        warmup_bias_lr=0.0,
-        use_fe=use_fe,
-        name=name,
-        iou_type="ciou",
-        mosaic=mosaic,
-        scale=scale,
-        use_dist=use_dist,
-        dist=d,
-        resume = False
-    )
+model = YOLO(model_path)
+name = f"{data_path}-{model_path}-{epochs}e-{optimizer}-scale{scale}-mosaic{mosaic}-d{d}_"
+model.train(
+    data=data_path,
+    epochs=epochs,
+    device=device,
+    optimizer=optimizer,
+    momentum=0.9,
+    lr0=0.01,
+    warmup_bias_lr=0.0,
+    use_fe=use_fe,
+    name=name,
+    iou_type="ciou",
+    mosaic=mosaic,
+    scale=scale,
+    use_dist=use_dist,
+    dist=d,
+    resume = False
+)
