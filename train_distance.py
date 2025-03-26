c3d436305c742e8338758234727308b1227e7274
asoderlind <axesode@gmail.com>
2025-03-26 09:08:27 +0100

    merge

--- train_distance.py
+++ train_distance.py
@@ -12,26 +12,29 @@ optimizer = "SGD"
 scale = 0.0
 mosaic = 1.0
 use_dist = True
-d = 0.35
+ds = [0.01, 0.1, 0.5, 1.0, 2.0, 5.0, 10.0]
 
-# model_path = "/home/phoawb/repos/YOLO-DLT/runs/detect/kitti.yaml-yolo11l.pt-200e-SGD-scale0.0-mosaic1.0-d0.35_14/weights/last.pt"
+model_path = "./weights/kitti-dist.pt"
 # model_path=f"{name}/weights/last.py"
 model = YOLO(model_path)
 name = f"{data_path}-{model_path}-{epochs}e-{optimizer}-scale{scale}-mosaic{mosaic}-d{d}_"
-model.train(
-    data=data_path,
-    epochs=epochs,
-    device=device,
-    optimizer=optimizer,
-    momentum=0.9,
-    lr0=0.01,
-    warmup_bias_lr=0.0,
-    use_fe=use_fe,
-    name=name,
-    iou_type="ciou",
-    mosaic=mosaic,
-    scale=scale,
-    use_dist=use_dist,
-    dist=d,
-    resume = False
-)
+for d in ds:
+    model = YOLO(model_path)
+    name = f"{data_path}-{model_path}-{epochs}e-{optimizer}-scale{scale}-mosaic{mosaic}-d{d}_"
+    model.train(
+        data=data_path,
+        epochs=epochs,
+        device=device,
+        optimizer=optimizer,
+        momentum=0.9,
+        lr0=0.01,
+        warmup_bias_lr=0.0,
+        use_fe=use_fe,
+        name=name,
+        iou_type="ciou",
+        mosaic=mosaic,
+        scale=scale,
+        use_dist=use_dist,
+        dist=d,
+        resume=False,
+    )
