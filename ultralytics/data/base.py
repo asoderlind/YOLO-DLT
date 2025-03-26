567e6aaa52fecc8c4718aa29d2b510b5aa7f4c70
asoderlind <axesode@gmail.com>
2025-03-26 09:55:51 +0100

    fix classes arg for dist

--- ultralytics/data/base.py
+++ ultralytics/data/base.py
@@ -149,11 +149,15 @@ class BaseDataset(Dataset):
         for i in range(len(self.labels)):
             if include_class is not None:
                 cls = self.labels[i]["cls"]
+                if self.use_dist:
+                    distances = self.labels[i]["distances"]
                 bboxes = self.labels[i]["bboxes"]
                 segments = self.labels[i]["segments"]
                 keypoints = self.labels[i]["keypoints"]
                 j = (cls == include_class_array).any(1)
                 self.labels[i]["cls"] = cls[j]
+                if self.use_dist:
+                    self.labels[i]["distances"] = distances[j]
                 self.labels[i]["bboxes"] = bboxes[j]
                 if segments:
                     self.labels[i]["segments"] = [segments[si] for si, idx in enumerate(j) if idx]
