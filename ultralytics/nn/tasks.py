7508057be0258565907fea65facb717eaabcf715
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-03-27 12:00:15 +0100

    make sure stride calc works for regular and temporal models

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -348,7 +348,11 @@ class DetectionModel(BaseModel):
                     return self.forward(x)["one2many"]
                 return self.forward(x)[0] if isinstance(m, (Segment, Pose, OBB)) else self.forward(x)
 
-            m.stride = torch.tensor([s / x.shape[-2] for x in _forward(torch.zeros(1, ch, s, s))])  # forward
+            # The temporal model returns a tuple even during training
+            if isinstance(_forward(torch.zeros(1, ch, s, s)), tuple):
+                m.stride = torch.tensor([s / x.shape[-2] for x in _forward(torch.zeros(1, ch, s, s))[1]])
+            else:
+                m.stride = torch.tensor([s / x.shape[-2] for x in _forward(torch.zeros(1, ch, s, s))])  # forward
             self.stride = m.stride
             m.bias_init()  # only run once
         else:
