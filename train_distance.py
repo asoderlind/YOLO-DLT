9df56ab48edff1f3b6a47ed39e1c178b2e4fa797
asoderlind <axesode@gmail.com>
2025-03-31 19:11:43 +0200

    prepare for ablations for augs

--- train_distance.py
+++ train_distance.py
@@ -9,10 +9,14 @@ import torch
 # Defaults
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 327a1be6 (prepare for ablations for augs)
 DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu"
 KITTI_CLASSES = [0, 1, 2, 3, 4, 5, 6, 7]
 EPOCHS = 100
 OPTIMIZER = "SGD"
+<<<<<<< HEAD
 
 
 def train_with_distance(
@@ -211,19 +215,34 @@ for conf in confs:
     resume = model_path != "yolo11n.pt"
 
 >>>>>>> 6a83822a (mod8)
-    model = YOLO(model_path)
+=======
 
-    name = f"{data_path}-{model_path}-{epochs}e-{optimizer}-{'dist' if use_dist else 'noDist'}-scale{scale}-mosaic{mosaic}-noDontCare-d{d}_"
+
+def train_with_distance(
+    model_path: str = "yolo11n.pt",
+    data_path: str = "kitti.yaml",
+    d: float = 0.05,
+    use_dist: bool = True,
+    classes=KITTI_CLASSES,
+    scale: float = 0.0,
+    mosaic: float = 1.0,
+    device: str = DEVICE,
+    **kwargs,
+):
+    resume = model_path != "yolo11n.pt"
+>>>>>>> 327a1be6 (prepare for ablations for augs)
+    model = YOLO(model_path)
+    name = f"{data_path}-{model_path}-{EPOCHS}e-{OPTIMIZER}-{'dist' if use_dist else 'noDist'}-scale{scale}-mosaic{mosaic}-noDontCare-d{d}_"
 
     model.train(
         data=data_path,
-        epochs=epochs,
+        epochs=EPOCHS,
         device=device,
-        optimizer=optimizer,
+        optimizer=OPTIMIZER,
+        batch_size=16,
         momentum=0.9,
         lr0=0.01,
         warmup_bias_lr=0.0,
-        use_fe=use_fe,
         name=name,
         iou_type="ciou",
         mosaic=mosaic,
@@ -231,6 +250,21 @@ for conf in confs:
         use_dist=use_dist,
         dist=d,
         resume=resume,
+<<<<<<< HEAD
         classes=[0, 1, 2, 3, 4, 5, 6, 7],
+=======
+        classes=classes,
+        **kwargs,
+>>>>>>> b59a9049 (prepare for ablations for augs)
     )
+<<<<<<< HEAD
 >>>>>>> 5969bc06 (merge)
+=======
+
+
+# Augmentation ablations
+train_with_distance(scale=0.0, mosaic=0.0)
+train_with_distance(scale=0.0, mosaic=1.0)
+train_with_distance(scale=0.5, mosaic=0.0)
+train_with_distance(scale=0.5, mosaic=1.0)
+>>>>>>> 327a1be6 (prepare for ablations for augs)
