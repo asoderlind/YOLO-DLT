490acd3d11eaeeb9a57bab00959062d93cd6c8b1
asoderlind <axesode@gmail.com>
2025-03-31 21:37:57 +0200

    changes

--- train_distance.py
+++ train_distance.py
@@ -3,28 +3,10 @@ import torch
 
 
 # Defaults
-<<<<<<< HEAD
-device = "cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu"
-use_fe = False
-epochs = 100
-optimizer = "SGD"
-scale = 0.0
-mosaic = 1.0
-confs = [
-    {
-        'd': 0,
-        'useDist': False,
-        'dataPath': 'waymo-noConf.yaml',
-        'model_path': "runs/detect/waymo-noConf.yaml-yolo11n.pt-100e-SGD-noDist-scale0.0-mosaic1.0-noDontCare-d0_2/weights/last.pt",
-        'classes': [1, 2, 3, 4]
-    },
-]
-=======
 DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu"
 KITTI_CLASSES = [0, 1, 2, 3, 4, 5, 6, 7]
 EPOCHS = 100
 OPTIMIZER = "SGD"
->>>>>>> b59a904967bdcac4c2a2444715962bc03e916184
 
 
 def train_with_distance(
@@ -47,7 +29,7 @@ def train_with_distance(
         epochs=EPOCHS,
         device=device,
         optimizer=OPTIMIZER,
-        batch_size=16,
+        batch=16,
         momentum=0.9,
         lr0=0.01,
         warmup_bias_lr=0.0,
@@ -59,7 +41,6 @@ def train_with_distance(
         dist=d,
         resume=resume,
         classes=classes,
-        **kwargs,
     )
 
 
