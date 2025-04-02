bc5d0a390450d26c39c1579e7c4c34300cd726b7
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-04-03 01:20:17 +0200

    update nwd rule

--- ultralytics/utils/tal.py
+++ ultralytics/utils/tal.py
@@ -171,7 +171,7 @@ class TaskAlignedAssigner(nn.Module):
         effective_iou_type = (
             self.iou_type if (not self.iou_type.startswith("wiou")) or self.iou_type == "nwd" else "ciou"
         )
-        effective_iou_type = "ciou" if (self.iou_type.startswith("wiou") or self.iou_type == "nwd") else self.iou_type
+        effective_iou_type = "ciou" if (self.iou_type.startswith("wiou") or "nwd" in self.iou_type) else self.iou_type
         return bbox_iou(gt_bboxes, pd_bboxes, xywh=False, iou_type=effective_iou_type).squeeze(-1).clamp_(0)
 
     def select_topk_candidates(self, metrics, largest=True, topk_mask=None):
