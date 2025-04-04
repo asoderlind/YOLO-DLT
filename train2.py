4bce726fb95f6cdf22771a34372bf0a93d480f24
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-04-04 10:49:29 +0200

    update train2

--- train2.py
+++ train2.py
@@ -175,14 +175,15 @@ if __name__ == "__main__":
     #     model="runs/detect/bdd100k_night-yolo11n-spdconv-rfac3k2/weights/last.pt",
     #     resume=True,
     # )
-    train_model(
-        name="bdd100k_night-yolo11n-spdconv-repc3k2",
-        model="runs/detect/bdd100k_night-yolo11n-spdconv-repc3k2/weights/last.pt",
-        resume=True,
-    )
+    # train_model(
+    #     name="bdd100k_night-yolo11n-spdconv-repc3k2",
+    #     model="runs/detect/bdd100k_night-yolo11n-spdconv-repc3k2/weights/last.pt",
+    #     resume=True,
+    # )
     train_model(
         name="bdd100k_night-yolo11n-spdconv-fa",
-        model="dlt-models/yolo11n-spdconv-fa.yaml",
+        model="runs/detect/bdd100k_night-yolo11n-spdconv-fa/weights/last.pt",
+        resume=True,
     )
     train_model(
         name="bdd100k_night-yolo11n-spdconv-cl",
