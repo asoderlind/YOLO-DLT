ec27cbde91edd081fb8a61a5d16605443a794501
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-04-08 15:21:44 +0200

    load pretrained

--- eovod_test.py
+++ eovod_test.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("dlt-models/yolo11n-EOVOD.yaml")
+model = YOLO("dlt-models/yolo11n-EOVOD.yaml").load("yolo11n.pt")
 # model.info(detailed=True)
 model.train(data="waymo16.yaml", epochs=1, batch=4, device="mps", temporal_window=2)
