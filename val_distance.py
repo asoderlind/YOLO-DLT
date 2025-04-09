1267296b390a1f5f4cc51dcf65fbfa1ca5de6402
asoderlind <axesode@gmail.com>
2025-04-09 12:07:31 +0200

    val dist

--- val_distance.py
+++ val_distance.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 import torch
 
 # PROD
-# model_path = "./runs/detect/kitti.yaml-yolo11n.pt-100e-SGD-dist-scale0.0-mosaic1.0-noDontCare-d0.05_/weights/best.pt"
+model_path = "./runs/detect/kitti.yaml-yolo11n.pt-100e-SGD-dist-scale0.0-mosaic1.0-noDontCare-d0.05_/weights/best.pt"
 
 # LOCAL
-model_path = "./weights/kitti.yaml-yolo11n.pt-100e-SGD-dist-scale0.0-mosaic1.0-noDontCare-d0.05_best.pt"
+#model_path = "./weights/kitti.yaml-yolo11n.pt-100e-SGD-dist-scale0.0-mosaic1.0-noDontCare-d0.05_best.pt"
 
 data_path = "kitti.yaml"
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
