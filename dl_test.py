559c2a000da5d35b5bdce79fba4ada31ccfb5ba1
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-04-24 10:21:50 +0200

    debug

--- dl_test.py
+++ dl_test.py
@@ -4,4 +4,6 @@ model = YOLO("dlt-models/yolo11n-temporal.yaml").load(
     "runs/detect/waymo-yolo11n-bdd100k_night-50e-lr0.001-lrf0.01-freeze0-SGD/weights/last.pt"
 )
 
-model.train(data="waymo16.yaml", device="cuda", batch=1, temporal_freeze=True, epochs=1)
+model2 = YOLO("dlt-models/yolo11n-temporal.yaml").load("yolo11n.pt")
+
+# model.train(data="waymo16.yaml", device="cuda", batch=1, temporal_freeze=True, epochs=1)
