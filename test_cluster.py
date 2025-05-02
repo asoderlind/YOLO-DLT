c7f4eee61a1858bf49a84f9b56b963fc7fc7efef
asoderlind <axesode@gmail.com>
2025-05-02 15:08:39 +0200

    add test cluster files

---
+++ test_cluster.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+import torch
+
+
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+
+"""
+Regular set with distance loss disabled
+"""
+model1 = YOLO("dlt-models/yolo11n.yaml")
+model1.train(
+    data="../ultralytics/cfg/datasets/bdd100k-cluster.yaml", epochs=1, device=device, use_fe=False, use_dist=False
+)
--- test_with_without_dist.py
+++ test_with_without_dist.py
@@ -8,16 +8,16 @@ device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is
 Regular set with distance loss disabled
 """
 model1 = YOLO("yolo11n.pt")
-model1.train(data="../ultralytics/cfg/datasets/coco8.yaml", epochs=1, device=device, use_fe=False, use_dist=False)
+model1.train(data="../ultralytics/cfg/datasets/bdd100k.yaml", epochs=1, device=device, use_fe=False, use_dist=False)
 
 """
 Distance set with distance loss enabled
 """
-model2 = YOLO("yolo11n.pt")
-model2.train(data="../ultralytics/cfg/datasets/kitti-mini.yaml", epochs=1, device=device, use_fe=False, use_dist=True)
+# model2 = YOLO("yolo11n.pt")
+# model2.train(data="../ultralytics/cfg/datasets/kitti-mini.yaml", epochs=1, device=device, use_fe=False, use_dist=True)
 
 """
 Distance set with distance loss disabled
 """
-model3 = YOLO("yolo11n.pt")
-model3.train(data="../ultralytics/cfg/datasets/kitti-mini.yaml", epochs=1, device=device, use_fe=False, use_dist=False)
+# model3 = YOLO("yolo11n.pt")
+# model3.train(data="../ultralytics/cfg/datasets/kitti-mini.yaml", epochs=1, device=device, use_fe=False, use_dist=False)
