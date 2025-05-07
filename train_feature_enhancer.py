4dac91310bddb9d4e7da504dca6302eb65a98f65
asoderlind <axesode@gmail.com>
2025-05-07 17:28:41 +0200

    update fe files

--- train_feature_enhancer.py
+++ train_feature_enhancer.py
@@ -1,21 +1,52 @@
 from ultralytics import YOLO
-from train_conf import (
-        MODEL,DEVICE, OPTIMIZER, MOMENTUM, BATCH, IOU_TYPE, LR0, WARMUP_BIAS_LR, PRETRAINED, EPOCHS
-        )
+from train_conf import MODEL, DEVICE, OPTIMIZER, MOMENTUM, BATCH, IOU_TYPE, LR0, WARMUP_BIAS_LR, PRETRAINED, EPOCHS
 
 
-def train_fe(model_name=MODEL, data="exDark-yolo.yaml", lc=0.5, fe=True, **kwargs):
-    name = f"{model_name}-{data}{'-fe-' if fe else '-noFe-'}e{epochs}-allLoss-lc{lc}-Auto-aug-preLoad_"
+def train_fe(model_name=MODEL, data_path="exDark-yolo.yaml", lc=0.5, use_fe=True, **kwargs):
+    name = f"{model_name}-{data_path}{'-fe-' if use_fe else '-noFe-'}-lc{lc}_"
     model = YOLO(MODEL)
     model.train(
-        data=data,
-        use_fe=fe,
-        epochs=EPOCHS,
-        device=DEVICE,
-        lambda_c=lc,
-        optimizer=OPTIMIZER,
         name=name,
+        data=data_path,
+        lambda_c=lc,
+        use_fe=use_fe,
+        device=DEVICE,
+        batch=BATCH,
+        momentum=MOMENTUM,
+        lr0=LR0,
+        iou_type=IOU_TYPE,
+        warmup_bias_lr=WARMUP_BIAS_LR,
+        optimizer=OPTIMIZER,
+        pretrained=PRETRAINED,
+        epochs=EPOCHS,
+        **kwargs,
     )
+
+
+train_fe(
+    data_path="bdd100k_night_mirnet.yaml",
+    use_fe=False,
+    lc=0.0,
+)
+
+train_fe(
+    data_path="bdd100k_night_mirnet.yaml",
+    use_fe=True,
+    lc=0.5,
+)
+
+train_fe(
+    data_path="bdd100k_night_mirnet.yaml",
+    use_fe=True,
+    lc=0.1,
+)
+
+train_fe(
+    data_path="bdd100k_night_mirnet.yaml",
+    use_fe=True,
+    lc=1.0,
+)
+
 
 """
 # With training only first layer then freezing
