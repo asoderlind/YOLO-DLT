7599d5d085e9636922426760bb630304f88b7232
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-05-08 12:17:00 +0200

    revert waymo script

--- scripts/wrangle_waymo.py
+++ scripts/wrangle_waymo.py
@@ -9,7 +9,7 @@ import dask.dataframe as dd
 import matplotlib.pyplot as plt  # type: ignore
 import numpy as np
 import pandas as pd  # type: ignore
-import tensorflow as tf  # type: ignore
+from gcsfs import GCSFileSystem  # type: ignore
 from tqdm import tqdm  # type: ignore
 
 pd.set_option("display.max_columns", None)
@@ -144,10 +144,9 @@ def read(
         paths = [path]
     else:
         # Read all contexts for this component
-        # fs = GCSFileSystem()
-        # paths = fs.glob(f"{DATASET_BUCKET}/{split}/{tag}/*.parquet")
-        # paths = [f"gs://{path}" for path in paths]
-        paths = tf.io.gfile.glob(f"{DATASET_BUCKET}/{split}/{tag}/*.parquet")
+        fs = GCSFileSystem()
+        paths = fs.glob(f"{DATASET_BUCKET}/{split}/{tag}/*.parquet")
+        paths = [f"gs://{path}" for path in paths]
 
     # Create the Dask DataFrame
     df = dd.read_parquet(
