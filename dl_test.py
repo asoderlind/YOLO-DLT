5be70b86f48abecccb239f3a08ad7a9a9b823e8e
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-05-16 19:08:54 +0200

    Test load again

--- dl_test.py
+++ dl_test.py
@@ -1,6 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO("dlt-models/yolo11n-temporal.yaml")
+model = YOLO("dlt-models/yolo11n-temporal.yaml").load(
+    "runs/detect/waymo-yolo11n-bdd100k_night-50e-lr0.001-lrf0.01-freeze0-SGD/weights/last.pt"
+)
 model.train(
     data="waymo16.yaml",
     epochs=1,
