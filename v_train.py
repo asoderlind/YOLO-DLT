0a8fd40bc41fb32ccae94ce50302b4a8d6fd337d
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-05-19 02:19:57 +0200

    u v train

--- v_train.py
+++ v_train.py
@@ -140,18 +140,17 @@ if __name__ == "__main__":
     #     close_mosaic=0,
     # )
     train_model_v(
-        name="waymo_night-yolo11n-4_gframe-0.005_lr0-0_temporal_cls",
+        name="waymo_night-yolo11n-4_gframe-0.005_lr0-5_temporal_cls",
         model="dlt-models/yolo11n-temporal.yaml",
         model_load_path="runs/detect/waymo-yolo11n-bdd100k_night-50e-lr0.001-lrf0.01-freeze0-SGD/weights/last.pt",
-        gframe=4,
-        epochs=10,
+        # gframe=4,
+        epochs=109,
         cos_lr=True,
         data="waymo_night.yaml",
-        lr0=0.01,
-        temporal_cls=0.0,
-        batch=1,
+        lr0=0.005,
+        temporal_cls=5.0,
+        batch=16,
         # temporal_freeze=True,
-        close_mosaic=0,
     )
     # train_model_v(
     #     name="waymo_night-yolo11nv-4_gframe-0.005_lr0-5_temporal_cls",
