c0a5fdb711dcf752f037c946c91a03ab2c5a3083
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-05-21 21:09:30 +0200

    test comment

--- scripts/wrangle_waymo.py
+++ scripts/wrangle_waymo.py
@@ -22,6 +22,7 @@ MAX_DISTANCE = 85.0
 IMAGE_WIDTH = 1920
 IMAGE_HEIGHT = 1280
 DEFAULT_DATASET_PATH = "/mnt/machine-learning-storage/ML1/ClusterOutput/MLC-499/Datasets/GENAI-6807_Waymo/"
+# test comment
 # DEFAULT_DATASET_PATH = "../yolo-testing/datasets/"
 
 
