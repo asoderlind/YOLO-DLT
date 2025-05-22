a1c3ee034d0c8650fc270b2e8523b8691c57ecd1
asoderlind <axesode@gmail.com>
2025-05-22 15:40:11 +0200

    add val for cluster

--- val_fe.py
+++ val_fe.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import torch
 import os
+from train_conf import CLUSTER_OUTPUT_PATH
 
 DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 ENV = os.getenv("ENV", "LOCAL")
@@ -18,32 +19,26 @@ def eval_model(model_path, data_path, **kwargs):
     )
 
 
-
 if __name__ == "__main__":
+    ###################
+    # BDD100k-cluster #
+    ###################
+
+    for s in [0, 1, 2]:
+        eval_model(f"{CLUSTER_OUTPUT_PATH}/bdd100k_night_cluster-yolo11n-seed-{s}/weights/best.pt", "bdd100k_night_cluster.yaml")
+
     ##########
     # EXDARK #
     ##########
-    for enhancer in ["dln", "mbllen"]:
-        for lc in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-            for s in [1, 2]:
-                eval_model(f"runs/detect/exDark-yolo-{enhancer}-c-yolo11n.yaml-200e-{s}s-noDist-d=0.05-fe-lc={lc}_/weights/best.pt", "exDark-yolo-dln.yaml")
-
-    #########
-    # CARLA #
-    #########
     """
-    eval_distance(
-        "runs/detect/carla-town06-night.yaml-yolo11n.pt-100e-SGD-dist-scale0.0-mosaic1.0-c-d0.05_/weights/best.pt",
-        "carla-town06-night.yaml",
-        True,
-        max_dist=100,
-    )
-    eval_distance(
-        "runs/detect/carla-town06-night.yaml-yolo11n.pt-200e-SGD-dist-scale0.0-mosaic1.0-c-d0.05_/weights/best.pt",
-        "carla-town06-night.yaml",
-        True,
-        max_dist=100,
-    )
+    for enhancer in ["mbllen"]:
+        for lc in [0.7, 0.8, 0.9, 1.0]:
+            for s in [1, 2]:
+                eval_model(f"runs/detect/exDark-yolo-{enhancer}/yolo11n.yaml-c-200e-noDist-d=0.05-fe-lc={lc}-s={s}_/weights/best.pt", f"exDark-yolo-{enhancer}.yaml")
+
+    s = 2
+    lc = 0.8
+    eval_model(f"runs/detect/exDark-yolo-dln-c-yolo11n.yaml-200e-{s}s-noDist-d=0.05-fe-lc={lc}_/weights/best.pt", "exDark-yolo-dln.yaml")
     """
 
     #########
@@ -114,6 +109,12 @@ if __name__ == "__main__":
     # WAYMO #
     #########
     '''
+    eval_model(
+        "runs/detect/waymo-noConf.yaml-dlt-models-yolo11n.yaml-100e-noPre-SGD-dist-scale0.0-mosaic1.0-c-d0.05_/weights/best.pt",
+        "waymo-noConf.yaml",
+        use_dist=True,
+        max_dist=85,
+    )
 
     eval_distance(
         "runs/detect/waymo-noConf.yaml-dlt-models-yolo11n.yaml-200e-SGD-noDist-scale0.5-mosaic1.0-c-d0.0_/weights/best.pt",
@@ -140,19 +141,7 @@ if __name__ == "__main__":
         max_dist=85,
     )
     eval_distance(
-        "runs/detect/waymo-noConf.yaml-runs-detect-carla-town06-all-night.yaml-dlt-models-yolo11n.yaml-100e-noPre-SGD-noDist-scale0.5-mosaic1.0-c-d0.0_-weights-best.pt-100e-noPre-SGD-noDist-scale0.5-mosaic1.0-c-d0.0_/weights/best.pt",
-        "waymo-noConf.yaml",
-        True,
-        max_dist=85,
-    )
-    eval_distance(
         "runs/detect/waymo-noConf.yaml-dlt-models-yolo11n-bic-reduced-channel.yaml-100e-noPre-SGD-dist-scale0.0-mosaic1.0-c-d0.05_/weights/best.pt",
-        "waymo-noConf.yaml",
-        True,
-        max_dist=85,
-    )
-    eval_distance(
-        "runs/detect/waymo-noConf.yaml-dlt-models-yolo11n.yaml-100e-noPre-SGD-dist-scale0.0-mosaic1.0-c-d0.05_/weights/best.pt",
         "waymo-noConf.yaml",
         True,
         max_dist=85,
