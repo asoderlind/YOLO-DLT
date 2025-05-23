b820e8273fac1ff7f6a1c6ceb2fbd2143d7bf49e
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-05-23 11:24:42 +0200

    correct name

--- sppf_runs.py
+++ sppf_runs.py
@@ -206,33 +206,33 @@ def run_seeded_val(
 if __name__ == "__main__":
     classes = 0
     run_seeded_train(
-        model_name="waymo_dark-yolo11n-bic-repc3k2-simsppf",
+        name="waymo_dark-yolo11n-bic-repc3k2-simsppf",
         model="dlt-models/yolo11n-bic-repc3k2-simsppf.yaml",
         data="waymo_dark.yaml",
     )
     run_seeded_train(
-        model_name="waymo_dark-yolo11n-bic-repc3k2-sppfcsp",
+        name="waymo_dark-yolo11n-bic-repc3k2-sppfcsp",
         model="dlt-models/yolo11n-bic-repc3k2-sppfcsp.yaml",
         data="waymo_dark.yaml",
     )
     run_seeded_train(
-        model_name="waymo_dark-yolo11n-bic-repc3k2-simsppfcsp",
+        name="waymo_dark-yolo11n-bic-repc3k2-simsppfcsp",
         model="dlt-models/yolo11n-bic-repc3k2-simsppfcsp.yaml",
         data="waymo_dark.yaml",
     )
 
     run_seeded_val(
-        model_name="waymo_dark-yolo11n-bic-repc3k2-simsppf",
+        name="waymo_dark-yolo11n-bic-repc3k2-simsppf",
         data="waymo_dark.yaml",
         classes=classes,
     )
     run_seeded_val(
-        model_name="waymo_dark-yolo11n-bic-repc3k2-sppfcsp",
+        name="waymo_dark-yolo11n-bic-repc3k2-sppfcsp",
         data="waymo_dark.yaml",
         classes=classes,
     )
     run_seeded_val(
-        model_name="waymo_dark-yolo11n-bic-repc3k2-simsppfcsp",
+        name="waymo_dark-yolo11n-bic-repc3k2-simsppfcsp",
         data="waymo_dark.yaml",
         classes=classes,
     )
