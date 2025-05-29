83f0a86a1871208148ba208e700f61d54165f979
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-05-29 02:41:41 +0200

    comment debug log

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -1,8 +1,6 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-import csv
 import json
-import os
 import random
 import time
 from collections import defaultdict
@@ -559,22 +557,22 @@ class TemporalYOLODataset(YOLODataset):
         #         f"TOTAL DATA TIME: {total_time:.2f} ms "
         #     )
 
-        filename = (
-            "train_temporal_dataset_frame_indices.tsv"
-            if self.mode == "train"
-            else "val_temporal_dataset_frame_indices.tsv"
-        )
+        # filename = (
+        #     "train_temporal_dataset_frame_indices.tsv"
+        #     if self.mode == "train"
+        #     else "val_temporal_dataset_frame_indices.tsv"
+        # )
 
-        # Write header if file doesn't exist
-        if not os.path.exists(filename):
-            with open(filename, "w", newline="") as f:
-                writer = csv.writer(f, delimiter="\t")
-                writer.writerow(["Index", "Video_ID", "Sequence_Indices"])
+        # # Write header if file doesn't exist
+        # if not os.path.exists(filename):
+        #     with open(filename, "w", newline="") as f:
+        #         writer = csv.writer(f, delimiter="\t")
+        #         writer.writerow(["Index", "Video_ID", "Sequence_Indices"])
 
-        # Append data
-        with open(filename, "a", newline="") as f:
-            writer = csv.writer(f, delimiter="\t")
-            writer.writerow([index, vid_id, sequence_indices])
+        # # Append data
+        # with open(filename, "a", newline="") as f:
+        #     writer = csv.writer(f, delimiter="\t")
+        #     writer.writerow([index, vid_id, sequence_indices])
         return batch
 
     @staticmethod
