22cffe269b17155bc2c9822584f701e6d7a81c1a
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-06-03 21:27:24 +0200

    name

--- v_val.py
+++ v_val.py
@@ -6,7 +6,7 @@ model = YOLO(
 
 model.val(
     name="waymo_dark-yolo11n-40_e-16_gframe-0.005_lr0-0.75_nms-temporal_dl-25.0_attn_scale-fixed-val",
-    data="waymo_night.yaml",
+    data="waymo_dark.yaml",
     batch=1,
     gframe=16,
     dataset_type="temporal",
