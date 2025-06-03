9ad974e7fdc2c7790d63d4f8ff1741556916ccc6
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-06-03 21:40:01 +0200

    preload test

--- v_train.py
+++ v_train.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
     # All runs have 16 nbs, 10% warmup, 4.0 gain, freeze true, cos_lr,
 
     train_model_v(
-        name="waymo_dark-yolo11n-40_e-16_gframe-0.005_lr0-0.75_nms-temporal_dl-25.0_attn_scale-fixed",
+        name="waymo_dark-yolo11n-40_e-16_gframe-0.005_lr0-0.75_nms-temporal_dl-25.0_attn_scale-fixed-preload_vid_cls",
         model="dlt-models/yolo11n-temporal-nms-0.75.yaml",
         model_load_path=WAYMO_TEMPORAL_BASE,
         data="waymo_dark.yaml",
@@ -148,6 +148,23 @@ if __name__ == "__main__":
         lr0=0.005,
         nbs=16,
     )
+
+    # train_model_v(
+    #     name="waymo_dark-yolo11n-40_e-16_gframe-0.005_lr0-0.75_nms-temporal_dl-25.0_attn_scale-fixed",
+    #     model="dlt-models/yolo11n-temporal-nms-0.75.yaml",
+    #     model_load_path=WAYMO_TEMPORAL_BASE,
+    #     data="waymo_dark.yaml",
+    #     gframe=16,
+    #     batch=1,
+    #     temporal_freeze=True,
+    #     temporal_cls=4.0,
+    #     epochs=40,
+    #     warmup_epochs=4,
+    #     cos_lr=True,
+    #     dataset_type="temporal",
+    #     lr0=0.005,
+    #     nbs=16,
+    # )
 
     # model_temp = YOLO(
     #     "runs/detect/waymo_dark-yolo11n-40_e-8_lframe-8_gframe-2_tmp_stride-0.001_lr0-0.75_nms-temporal_dl-mosaic/weights/last.pt"
