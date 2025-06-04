5913e91b6f90f802381612cf944d28d2009cd3bb
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-06-04 03:00:34 +0200

    val

--- v_val.py
+++ v_val.py
@@ -1,14 +1,13 @@
 from ultralytics import YOLO
 
-model = YOLO(
-    "runs/detect/waymo_dark-yolo11n-40_e-16_gframe-0.005_lr0-0.75_nms-temporal_dl-25.0_attn_scale-fixed/weights/last.pt"
-)
+model = YOLO("runs/detect/waymo_dark-yolo11n-40_e-8_lframe-8_gframe-mosaic-fixed/weights/last.pt")
 
 model.val(
-    name="waymo_dark-yolo11n-40_e-16_gframe-0.005_lr0-0.75_nms-temporal_dl-25.0_attn_scale-fixed-val",
+    name="waymo_dark-yolo11n-40_e-8_lframe-8_gframe-mosaic-fixed-val",
     data="waymo_dark.yaml",
     batch=1,
-    gframe=16,
+    gframe=8,
+    lframe=8,
     dataset_type="temporal",
     device="cuda",
 )
