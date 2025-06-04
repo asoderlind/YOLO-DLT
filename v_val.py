16e4a4ed00abfebcc067c2a83eac508f4f1d1168
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-06-04 12:57:13 +0200

    u vval

--- v_val.py
+++ v_val.py
@@ -1,19 +1,21 @@
 from ultralytics import YOLO
 
-model = YOLO(
-    "runs/detect/waymo_dark-yolo11n-40_e-16_gframe-0.005_lr0-0.75_nms-temporal_dl-25.0_attn_scale-fixed-preload_vid_cls-full_freeze/weights/last.pt"
-)
+model_name = "waymo_dark-yolo11n-40_e-8_lframe-8_gframe-mosaic-fixed"
+
+model = YOLO(f"runs/detect/{model_name}/weights/last.pt")
 
 # WAYMO_TEMPORAL_BASE = "runs/detect/waymo_dark-yolo11n-temporal-base/weights/last.pt"
 # model = YOLO(WAYMO_TEMPORAL_BASE)
 
-model.val(
-    # name="waymo_dark-yolo11n-40_e-16_gframe-0.005_lr0-0.75_nms-temporal_dl-0.25_attn_scale3",
-    name="waymo_dark-yolo11n-40_e-16_gframe-0.005_lr0-0.75_nms-temporal_dl-25.0_attn_scale-fixed-preload_vid_cls-full_freeze-val-seed",
-    data="waymo_dark.yaml",
-    batch=1,
-    gframe=16,
-    dataset_type="temporal",
-    device="cuda",
-    seed=1,
-)
+for i in range(1, 3):
+    model.val(
+        # name="waymo_dark-yolo11n-40_e-16_gframe-0.005_lr0-0.75_nms-temporal_dl-0.25_attn_scale3",
+        name=f"{model_name}-val-seed-{i}",
+        data="waymo_dark.yaml",
+        batch=1,
+        gframe=8,
+        lframe=8,
+        dataset_type="temporal",
+        device="cuda",
+        seed=i,
+    )
