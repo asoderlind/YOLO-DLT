39e49b8d67a3bbc9cfa52a72a2925b9ba44af81c
Philip Welin-Berger <philip.welin.berger@gmail.com>
2025-06-05 01:47:58 +0200

    unfreeze model

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -129,7 +129,6 @@ class Detect(nn.Module):
             return dbox.transpose(1, 2), cls.sigmoid().permute(0, 2, 1)
         else:
             dbox = self.decode_bboxes(self.dfl(box), self.anchors.unsqueeze(0)) * self.strides
-
         return torch.cat((dbox, cls.sigmoid()), 1)
 
     def bias_init(self):
@@ -532,13 +531,13 @@ class TemporalDetect(Detect):
                 param.requires_grad = False
             for param in self.cv3[i].parameters():
                 param.requires_grad = False
-            # for param in self.cv2_pred[i].parameters():
-            #     param.requires_grad = False
-            # for param in self.cv3_pred[i].parameters():
-            #     param.requires_grad = False
-            # if self.vid_cls:
-            #     for param in self.vid_cls[i].parameters():
-            #         param.requires_grad = False
+            for param in self.cv2_pred[i].parameters():
+                param.requires_grad = False
+            for param in self.cv3_pred[i].parameters():
+                param.requires_grad = False
+            if self.vid_cls:
+                for param in self.vid_cls[i].parameters():
+                    param.requires_grad = False
 
     def generate_cv3_to_vid_cls_mapping(self):
         """Generate mapping from cv3 to vid_cls branches"""
